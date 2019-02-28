67817d2251ac4d1c73d9ac01b9552a5d227bae79
Ishika <ishikabtr@gmail.com>
2019-02-28 04:25:44 +0000

    Add training/testing part

--- lab2.py
+++ lab2.py
@@ -6,20 +6,64 @@ from layer import Excitatory_Layer as EL
 from sklearn.datasets import fetch_mldata
 import firstlayer as firstlayer
 import layer as layer
+import time
+import gzip
+import pickle as pkl
 
-mnist = fetch_mldata('MNIST original')
-N, _ = mnist.data.shape
+#mnist = fetch_mldata('MNIST original')
+path = 'mnist.pkl.gz'
+train_X = None
+train_y = None
+with gzip.open(path, 'rb') as f:
+    (train_X, train_y), (val_X, val_y), (test_X, test_y) = pkl.load(f, encoding='latin1')
+            
+N, _ = train_X.shape
 
 # Reshape the data to be square
-mnist.square_data = mnist.data.reshape(N,28,28)
-layer1 = firstlayer.FirstLayer(1, mnist.square_data, mnist.target)
+np.random.seed(2)
 
-# You will need to change the instantiation of this layer
-# in order to properly initialize a new layer
-layer2 = IL(2, layer1, 6, 12)
-layer3 = EL(3, layer2, 2, 12)
+shuffledindex = np.random.permutation([i for i in (range(N))])
+mnistdata = train_X[shuffledindex,:]
+mnisttarget = train_y[shuffledindex]
 
-layer2.process_image('LowPass')
-layer3.process_image()
+square_data = mnistdata.reshape(N,28,28)
+num_training_imgs = 10000
+
+layer1 = firstlayer.FirstLayer(1)
+layer2 = IL(layer_id=1,
+            prev_layer=layer1,
+            threshold=3,
+            receptive_field=12)
+
+layer3 = EL(input_dim=8,
+            output_dim=16,
+            layer_id=3,
+            prev_layer=layer2,
+            threshold=2,  
+            initial_weight=1)
+layer4 = LL(layer_id=4,
+            prev_layer=layer3,
+            threshold=None,
+            receptive_field=None)
+#layer1_train = firstlayer.FirstLayer(1, square_data[0:num_training_imgs - 1], mnisttarget[0:num_training_imgs - 1])
+
+x1 = layer1.forward(mnist.square_data[-10:], 12)
+x2 = layer2.forward(x1, mode='Exact')
+x3 = layer3.forward(x2)
+x4 = layer4.forward(data=x3)
+print (x1)
+print (x2)
+print (x3)
+print (x4)
+
+#Update Weights of a certain layer:
+layer3 = stdp_update_rule(layer3, x4)
+
 
 layer3.write_spiketimes('spiketimes.csv', layer3.output)
+
+#num_testing_imgs = 10000
+
+
+
+
