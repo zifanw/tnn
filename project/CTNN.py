2ecd79e35152a6ef1231e509505e64f2fe8918b5
SailColubrid <wangzifansail@icloud.com>
2019-04-17 11:36:49 -0700

    update cifar

--- project/CTNN.py
+++ project/CTNN.py
@@ -6,20 +6,19 @@ import SpykeTorch.utils as utils
 import torchvision.transforms as transforms
 import torch
 from torch.utils.data import Dataset, DataLoader, TensorDataset
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, CIFAR
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 from tqdm import tqdm, trange
 
 use_cuda = True
 MAX_EPOCH = 1
-SMALL = 20000
 
 class InputTransform:
    def __init__(self, filter):
       self.to_tensor = transforms.ToTensor()
       self.filter = filter
-      self.temporal_transform = utils.Intensity2Latency(15, to_spike=True)
+      self.temporal_transform = utils.Intensity2Latency(30, to_spike=True)
    def __call__(self, image):
       image = self.to_tensor(image) * 255
       image.unsqueeze_(0)
@@ -32,12 +31,11 @@ class CTNN(nn.Module):
     def __init__(self):
         super(CTNN, self).__init__()
         self.conv1 = snn.Convolution(2, 30, 5, 0.8, 0.02)  #(in_channels, out_channels, kernel_size, weight_mean=0.8, weight_std=0.02)
-        self.conv2 = snn.Convolution(30, 100, 5, 0.8, 0.05)
+        self.conv2 = snn.Convolution(30, 100, 5, 0.8, 0.02)
         #self.conv3 = snn.Convolution(250, 200, 5, 0.8, 0.05)
 
         self.stdp1 = snn.STDP(self.conv1, (0.004, -0.003))
         self.stdp2 = snn.STDP(self.conv2, (0.004, -0.003))
-        self.training = True
         self.ctx = {"input_spikes": None, "potentials": None, "output_spikes":None, "winners":None}
 
     def save_data(self, input_spk, pot, spk, winners):
@@ -53,7 +51,7 @@ class CTNN(nn.Module):
             spk, pot = sf.fire(pot, 15, True)
             pot = sf.pointwise_inhibition(pot) # inter-channel inhibition
             if max_layer == 1:
-                winners = sf.get_k_winners(pot, 5, 3)
+                winners = sf.get_k_winners(pot, 8, 3)
                 self.save_data(input, pot, spk, winners)
                 return spk, pot
             spk_in = sf.pad(sf.pooling(spk, 2, 2), (1,1,1,1))
@@ -61,7 +59,7 @@ class CTNN(nn.Module):
             spk, pot = sf.fire(pot, 10, True)
             pot = sf.pointwise_inhibition(pot) # inter-channel inhibition
             if max_layer == 2:
-                winners = sf.get_k_winners(pot, 8, 2)
+                winners = sf.get_k_winners(pot, 5, 8)
                 self.save_data(spk_in, pot, spk, winners)
                 return spk, pot
         else:
@@ -125,8 +123,38 @@ def GMP(data):
     _, C, _, _ = data.shape
     final_pot = data[-1]
     max_pool = np.max(final_pot, axis=(1,2))
-    return max_pool.reshape((1,C))
+    return max_pool.reshape((1,C)) 
 
+def sample_zero_mean(x):
+    """
+    Make each sample have a mean of zero by subtracting mean along the feature axis.
+    :param x: float32(shape=(samples, features))
+    :return: array same shape as x
+    """
+    return x - x.mean(axis=1, keepdims=True)
+    pass
+
+
+def gcn(x, scale=1., bias=0.01):
+    """
+    GCN each sample (assume sample mean=0)
+    :param x: float32(shape=(samples, features))
+    :param scale: factor to scale output
+    :param bias: bias for sqrt
+    :return: scale * x / sqrt(bias + sample variance)
+    """
+    var = np.var(x, axis=1, keepdims=True)
+    return scale * x / np.sqrt(bias + var)
+    pass
+
+
+
+def preprocess(x, xtest):
+    x = sample_zero_mean(x)
+    x = gcn(x)
+    xtest = sample_zero_mean(xtest)
+    xtest = gcn(xtest)
+    return x, xtest
 
 if __name__ == "__main__":
 
@@ -137,32 +165,35 @@ if __name__ == "__main__":
     transform = InputTransform(filter)
 
     data_root = 'data/'
+    cifar_data_root = 'cifar/'
 
-    MNIST_train = utils.CacheDataset(MNIST(root=data_root, train=True, download=True, transform=transform))
-    MNIST_test = utils.CacheDataset(MNIST(root=data_root, train=False, download=True, transform=transform))
+    # MNIST_train = utils.CacheDataset(MNIST(root=data_root, train=True, download=True, transform=transform)) # 60000 x 28 x 28
+    # MNIST_test = utils.CacheDataset(MNIST(root=data_root, train=False, download=True, transform=transform)) # 10000 x 28 
+
+    # MNIST_loader = DataLoader(MNIST_train, batch_size=1000, shuffle=True)
+    # MNIST_test_loader = DataLoader(MNIST_test, batch_size=1000, shuffle=False)
+
+    CIFAR_train = utils.CacheDataset(CIFAR(root=cifar_data_root, train=True, download=True, transform=transform)) # 60000 x 30 x 30
+    CIFAR_test = utils.CacheDataset(CIFAR(root=cifar_data_root, train=False, download=True, transform=transform)) # 10000 x 30
+
+    CIFAR_loader = DataLoader(CIFAR_train, batch_size=1000, shuffle=True)
+    CIFAR_test_loader = DataLoader(CIFAR_test, batch_size=1000, shuffle=False)
     
-    MNIST_small = torch.utils.data.random_split(MNIST_train, lengths=[SMALL, len(MNIST_train)-SMALL])[0]
-
-    MNIST_loader = DataLoader(MNIST_train, batch_size=1000, shuffle=True)
-    MNIST_test_loader = DataLoader(MNIST_test, batch_size=1000, shuffle=False)
-    MNIST_small_loader = DataLoader(MNIST_small, batch_size=500, shuffle=True)
 
     net = CTNN()
-    clf = svm.SVC()
+    clf = svm.SVC(verbose=True) # (60000 x 100)
 
-    #net = train(net, MNIST_loader)
-    #torch.save(net.state_dict(), "./checkpoint.pt")
-    net.state_dict(torch.load("./checkpoint.pt"))
-    train_outputs, train_y = inference(net, MNIST_loader)
+    net = train(net, MNIST_loader)
+    torch.save(net.state_dict(), "./checkpoint.pt")
+    net.state_dict(torch.load("./cifar_checkpoint.pt"))
+    train_outputs, train_y = inference(net, CIFAR_loader)
+    test_outputs, test_y  = inference(net, CIFAR_test_loader)
+    train_outputs, test_outputs = preprocess(train_outputs, test_outputs)
     
-    train_mean = train_outputs.mean()
-    train_outputs -= train_mean
     clf.fit(train_outputs, train_y)
     acc=clf.score(train_outputs, train_y)
     print ("Training Accuracy is %.3f" % acc)
-    test_outputs, test_y  = inference(net, MNIST_test_loader)
-    test_outputs -= train_mean
-    
+   
     acc = clf.score(test_outputs, test_y)
     print ("Test Accuracy is %.3f" % acc)
     
